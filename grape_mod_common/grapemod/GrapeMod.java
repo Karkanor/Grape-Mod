d76ab2d5c0bcbb694736f05ff10ff56329f19837
karkanor <ethantd123@gmail.com>
2013-05-31 22:36:20 -0400

    Mod ID

---
+++ grape_mod_common/grapemod/GrapeMod.java
@@ -0,0 +1,11 @@
+package grapemod;
+
+import grapemod.lib.Reference;
+import cpw.mods.fml.common.Mod;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+public class GrapeMod {
+
+    @PreInit
+    public void 
+}
---
+++ grape_mod_common/grapemod/lib/Reference.java
@@ -0,0 +1,8 @@
+package grapemod.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "Winecraft";
+    public static final String MOD_NAME = "Winecraft";
+    public static final String VERSION = "0.0.1";
+}
